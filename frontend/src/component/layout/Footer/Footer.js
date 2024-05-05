a8928ecf4f782ee3b82295540ebe14876773eb51
sagar_patel <sagar.bcpatel@gmail.com>
2024-05-05 18:07:22 +0530

    changes in footer

--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -14,10 +14,10 @@ const Footer = () => {
       </div>
 
       <div className="midFooter">
-        <h1>ECOMMERCE.</h1>
+        <h1>FYNTRA</h1>
         <p>High Quality is our first priority</p>
 
-        <p>Copyrights 2021 &copy; MeAbhiSingh</p>
+        <p>Copyrights 2021 &copy; </p>
       </div>
 
       <div className="rightFooter">
